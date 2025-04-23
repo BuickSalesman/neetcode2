a3a745243cd1a270e8ca0ac6be0621af148d0519
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-23 14:17:19 -0400

    solved products of array discluding self

--- productsOfArrayDiscludingSelf.js
+++ productsOfArrayDiscludingSelf.js
@@ -3,5 +3,22 @@ class Solution {
    * @param {number[]} nums
    * @return {number[]}
    */
-  productExceptSelf(nums) {}
+  productExceptSelf(nums) {
+    let res = Array.from({ length: nums.length }, () => 1);
+
+    let prefix = 1,
+      postfix = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+      res[i] = prefix;
+      prefix *= nums[i];
+    }
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+      res[i] *= postfix;
+      postfix *= nums[i];
+    }
+
+    return res;
+  }
 }
