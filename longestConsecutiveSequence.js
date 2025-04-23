51ffd8c924bf56179c8f69c48a48f5c94783ecb1
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-23 14:45:15 -0400

    solved longest-consecutive sequence

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -3,5 +3,25 @@ class Solution {
    * @param {number[]} nums
    * @return {number}
    */
-  longestConsecutive(nums) {}
+  longestConsecutive(nums) {
+    let numSet = {};
+    let longest = 0;
+
+    for (const num of nums) {
+      numSet[num] = true;
+    }
+
+    for (let num in numSet) {
+      num = parseInt(num);
+      if (!(num - 1 in numSet)) {
+        let length = 1;
+        while (num + length in numSet) {
+          length++;
+        }
+        longest = Math.max(length, longest);
+      }
+    }
+
+    return longest;
+  }
 }
