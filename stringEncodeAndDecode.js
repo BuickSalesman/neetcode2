27bce30917a64f14f9b046658ca4594b18390141
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-23 13:39:50 -0400

    solved encode decode

--- stringEncodeAndDecode.js
+++ stringEncodeAndDecode.js
@@ -3,11 +3,34 @@ class Solution {
    * @param {string[]} strs
    * @returns {string}
    */
-  encode(strs) {}
+  encode(strs) {
+    let res = "";
+
+    for (const string of strs) {
+      res += string.length + "#" + string;
+    }
+    return res;
+  }
 
   /**
    * @param {string} str
    * @returns {string[]}
    */
-  decode(str) {}
+  decode(str) {
+    const res = [];
+    let i = 0;
+
+    while (i < str.length) {
+      let j = i;
+      while (str[j] !== "#") {
+        j++;
+      }
+      let length = parseInt(str.substring(i, j));
+      i = j + 1;
+      j = i + length;
+      res.push(str.substring(i, j));
+      i = j;
+    }
+    return res;
+  }
 }
