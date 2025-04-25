397f7e3c8b75c6f3b7bdd8d7f70b3fba1f1286b8
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-25 14:25:17 -0400

    solved minimum sindow substring

--- minimumWindowSubstring.js
+++ minimumWindowSubstring.js
@@ -4,5 +4,46 @@ class Solution {
    * @param {string} t
    * @return {string}
    */
-  minWindow(s, t) {}
+  minWindow(s, t) {
+    if (t === "") return "";
+
+    let window = {},
+      countT = {};
+
+    for (let c of t) {
+      countT[c] = 1 + (countT[c] || 0);
+    }
+
+    let l = 0;
+    let have = 0,
+      need = Object.keys(countT).length;
+
+    let res = [-1, -1],
+      resLen = Infinity;
+
+    for (let r = 0; r < s.length; r++) {
+      let c = s[r];
+
+      window[c] = 1 + (window[c] || 0);
+
+      if (countT[c] && countT[c] === window[c]) {
+        have++;
+      }
+
+      while (have === need) {
+        if (r - l + 1 < resLen) {
+          resLen = r - l + 1;
+          res = [l, r];
+        }
+
+        window[s[l]]--;
+
+        if (countT[s[l]] && countT[s[l]] > window[s[l]]) {
+          have--;
+        }
+        l++;
+      }
+    }
+    return resLen === Infinity ? "" : s.slice(res[0], res[1] + 1);
+  }
 }
