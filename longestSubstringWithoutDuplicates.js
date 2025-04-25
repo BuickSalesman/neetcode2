f208792b54ee7a09db1d0e650429ac8d26adced5
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-25 12:33:51 -0400

    solved longest substring withput duplicates

--- longestSubstringWithoutDuplicates.js
+++ longestSubstringWithoutDuplicates.js
@@ -3,5 +3,18 @@ class Solution {
    * @param {string} s
    * @return {number}
    */
-  lengthOfLongestSubstring(s) {}
+  lengthOfLongestSubstring(s) {
+    let l = 0;
+    let seen = {};
+    let res = 0;
+
+    for (let r = 0; r < s.length; r++) {
+      if (s[r] in seen) {
+        l = Math.max(l, seen[s[r]] + 1);
+      }
+      seen[s[r]] = r;
+      res = Math.max(res, r - l + 1);
+    }
+    return res;
+  }
 }
