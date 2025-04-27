fae64efbf9d4cae667d89dc32aa3ba44388a12ff
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-27 15:59:27 -0400

    solved merge two sorted linked lists js

--- mergeTwoSortedLinkedLists.js
+++ mergeTwoSortedLinkedLists.js
@@ -14,5 +14,25 @@ class Solution {
    * @param {ListNode} list2
    * @return {ListNode}
    */
-  mergeTwoLists(list1, list2) {}
+  mergeTwoLists(list1, list2) {
+    let dummy = new ListNode();
+    let tail = dummy;
+
+    while (list1 && list2) {
+      if (list1.val < list2.val) {
+        tail.next = list1;
+        list1 = list1.next;
+      } else {
+        tail.next = list2;
+        list2 = list2.next;
+      }
+      tail = tail.next;
+    }
+    if (list1) {
+      tail.next = list1;
+    } else if (list2) {
+      tail.next = list2;
+    }
+    return dummy.next;
+  }
 }
