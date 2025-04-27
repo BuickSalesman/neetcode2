c2dc25e4cfe89b66a3dedb572f745700bd287252
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-26 22:52:55 -0400

    solved find minimum in roatated sorted array

--- findMinimumInRotatedSortedArray.js
+++ findMinimumInRotatedSortedArray.js
@@ -1,7 +1,17 @@
 class Solution {
-  /**
-   * @param {number[]} nums
-   * @return {number}
-   */
-  findMin(nums) {}
+  findMin(nums) {
+    let l = 0,
+      r = nums.length - 1;
+
+    while (l < r) {
+      let m = Math.floor(l + (r - l) / 2);
+
+      if (nums[m] < nums[r]) {
+        r = m;
+      } else {
+        l = m + 1;
+      }
+    }
+    return nums[l];
+  }
 }
