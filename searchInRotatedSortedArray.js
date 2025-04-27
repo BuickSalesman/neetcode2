43a938d94f4e2067c334f20855eda04542cc11fd
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-26 22:17:09 -0400

    solved find targer in rotated sorted array

--- searchInRotatedSortedArray.js
+++ searchInRotatedSortedArray.js
@@ -4,5 +4,33 @@ class Solution {
    * @param {number} target
    * @return {number}
    */
-  search(nums, target) {}
+  search(nums, target) {
+    let l = 0,
+      r = nums.length - 1;
+
+    while (l <= r) {
+      let mid = Math.floor((l + r) / 2);
+      if (nums[mid] === target) {
+        return mid;
+      }
+      if (nums[l] <= nums[mid]) {
+        if (nums[l] > target || nums[mid] < target) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      } else {
+        if (nums[r] < target || nums[mid] > target) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
 }
+
+// [3,4,5,6,0,1,2] t = 6
+//  l   m     r
+//  0     3     6
