096b68a78df8a43b41c075789b82ff7a9a222e24
BuickSalesman <adam.odonnell.e@gmail.com>
2025-04-30 20:11:24 -0400

    solved valid BST

--- validBinarySearchTree.js
+++ validBinarySearchTree.js
@@ -14,5 +14,19 @@ class Solution {
    * @param {TreeNode} root
    * @return {boolean}
    */
-  isValidBST(root) {}
+  isValidBST(root) {
+    return this.valid(root, -Infinity, Infinity);
+  }
+
+  valid(node, left, right) {
+    if (node === null) {
+      return true;
+    }
+
+    if (!(left < node.val && node.val < right)) {
+      return false;
+    }
+
+    return this.valid(node.left, left, node.val) && this.valid(node.right, node.val, right);
+  }
 }
