bc41aea17539d8f99dcd8d1b892e2a3bd7d9d3a5
BuickSalesman <adam.odonnell.e@gmail.com>
2025-05-02 22:27:54 -0400

    solved serialize and deserialize binary tree

--- serializeAndDeserializeBinaryTree.js
+++ serializeAndDeserializeBinaryTree.js
@@ -16,7 +16,21 @@ class Codec {
    * @param {TreeNode} root
    * @return {string}
    */
-  serialize(root) {}
+  serialize(root) {
+    const res = [];
+    this.dfsS(root, res);
+    return res.join(",");
+  }
+
+  dfsS(node, res) {
+    if (node === null) {
+      res.push("N");
+      return;
+    }
+    res.push(node.val.toString());
+    this.dfsS(node.left, res);
+    this.dfsS(node.right, res);
+  }
 
   /**
    * Decodes your encoded data to tree.
@@ -24,5 +38,22 @@ class Codec {
    * @param {string} data
    * @return {TreeNode}
    */
-  deserialize(data) {}
+  deserialize(data) {
+    const vals = data.split(",");
+    const i = { val: 0 };
+    return this.dfsDS(vals, i);
+  }
+
+  dfsDS(vals, i) {
+    if (vals[i.val] === "N") {
+      i.val++;
+      return null;
+    }
+
+    const node = new TreeNode(parseInt(vals[i.val]));
+    i.val++;
+    node.left = this.dfsDS(vals, i);
+    node.right = this.dfsDS(vals, i);
+    return node;
+  }
 }
