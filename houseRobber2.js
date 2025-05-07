260867df1cb7a6c046044415d6327ab90bc327de
BuickSalesman <adam.odonnell.e@gmail.com>
2025-05-06 20:24:10 -0400

    solved house robber 2

--- houseRobber2.js
+++ houseRobber2.js
@@ -3,5 +3,19 @@ class Solution {
    * @param {number[]} nums
    * @return {number}
    */
-  rob(nums) {}
+  rob(nums) {
+    return Math.max(nums[0], this.helper(nums.slice(1)), this.helper(nums.slice(0, -1)));
+  }
+
+  helper(nums) {
+    let rob1 = 0,
+      rob2 = 0;
+
+    for (const num of nums) {
+      let temp = Math.max(rob1 + num, rob2);
+      rob1 = rob2;
+      rob2 = temp;
+    }
+    return rob2;
+  }
 }
