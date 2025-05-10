47f0b2c22c90d23bea8bc0bb8acb81fb3a64efe6
BuickSalesman <adam.odonnell.e@gmail.com>
2025-05-09 21:37:36 -0400

    solved non overlapping intervals

--- nonOverlapingIntervals.js
+++ nonOverlapingIntervals.js
@@ -3,5 +3,18 @@ class Solution {
    * @param {number[][]} intervals
    * @return {number}
    */
-  eraseOverlapIntervals(intervals) {}
+  eraseOverlapIntervals(intervals) {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let res = 0;
+    let lastEnd = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+      if (lastEnd > intervals[i][0]) {
+        res++;
+      } else {
+        lastEnd = intervals[i][1];
+      }
+    }
+    return res;
+  }
 }
